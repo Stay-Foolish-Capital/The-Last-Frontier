24cbab9631907f6b009cdf698b1de1432ecad5eb
Stay-Foolish-Capital <160165584+Stay-Foolish-Capital@users.noreply.github.com>
2024-03-03 06:36:12 +0900

    Update mobile.js

--- mario/code/mobile.js
+++ mario/code/mobile.js
@@ -1,39 +1,39 @@
 const IsSButtonPressed = () => (document.getElementById('SButton').getAttribute('aria-pressed') === 'true');
 
-document.getElementById('SButton').addEventListener('mousedown', () => document.getElementById('SButton').setAttribute('aria-pressed', 'true'));
-document.getElementById('SButton').addEventListener('mouseup', () => document.getElementById('SButton').setAttribute('aria-pressed', 'false'));
+document.getElementById('SButton').addEventListener('touchstart', () => document.getElementById('SButton').setAttribute('aria-pressed', 'true'));
+document.getElementById('SButton').addEventListener('touchend', () => document.getElementById('SButton').setAttribute('aria-pressed', 'false'));
 
 
 
 const IsAButtonPressed = () => (document.getElementById('AButton').getAttribute('aria-pressed') === 'true');
 
-document.getElementById('AButton').addEventListener('mousedown', () => document.getElementById('AButton').setAttribute('aria-pressed', 'true'));
-document.getElementById('AButton').addEventListener('mouseup', () => document.getElementById('AButton').setAttribute('aria-pressed', 'false'));
+document.getElementById('AButton').addEventListener('touchstart', () => document.getElementById('AButton').setAttribute('aria-pressed', 'true'));
+document.getElementById('AButton').addEventListener('touchend', () => document.getElementById('AButton').setAttribute('aria-pressed', 'false'));
 
 
 
 const IsLeftButtonPressed = () => (document.getElementById('LeftButton').getAttribute('aria-pressed') === 'true');
 
-document.getElementById('LeftButton').addEventListener('mousedown', () => document.getElementById('LeftButton').setAttribute('aria-pressed', 'true'));
-document.getElementById('LeftButton').addEventListener('mouseup', () => document.getElementById('LeftButton').setAttribute('aria-pressed', 'false'));
+document.getElementById('LeftButton').addEventListener('touchstart', () => document.getElementById('LeftButton').setAttribute('aria-pressed', 'true'));
+document.getElementById('LeftButton').addEventListener('touchend', () => document.getElementById('LeftButton').setAttribute('aria-pressed', 'false'));
 
 
 
 const IsRightButtonPressed = () => (document.getElementById('RightButton').getAttribute('aria-pressed') === 'true');
 
-document.getElementById('RightButton').addEventListener('mousedown', () => document.getElementById('RightButton').setAttribute('aria-pressed', 'true'));
-document.getElementById('RightButton').addEventListener('mouseup', () => document.getElementById('RightButton').setAttribute('aria-pressed', 'false'));
+document.getElementById('RightButton').addEventListener('touchstart', () => document.getElementById('RightButton').setAttribute('aria-pressed', 'true'));
+document.getElementById('RightButton').addEventListener('touchend', () => document.getElementById('RightButton').setAttribute('aria-pressed', 'false'));
 
 
 
 const IsUpButtonPressed = () => (document.getElementById('UpButton').getAttribute('aria-pressed') === 'true');
 
-document.getElementById('UpButton').addEventListener('mousedown', () => document.getElementById('UpButton').setAttribute('aria-pressed', 'true'));
-document.getElementById('UpButton').addEventListener('mouseup', () => document.getElementById('UpButton').setAttribute('aria-pressed', 'false'));
+document.getElementById('UpButton').addEventListener('touchstart', () => document.getElementById('UpButton').setAttribute('aria-pressed', 'true'));
+document.getElementById('UpButton').addEventListener('touchend', () => document.getElementById('UpButton').setAttribute('aria-pressed', 'false'));
 
 
 
 const IsDownButtonPressed = () => (document.getElementById('DownButton').getAttribute('aria-pressed') === 'true');
 
-document.getElementById('DownButton').addEventListener('mousedown', () => document.getElementById('DownButton').setAttribute('aria-pressed', 'true'));
-document.getElementById('DownButton').addEventListener('mouseup', () => document.getElementById('DownButton').setAttribute('aria-pressed', 'false'));
+document.getElementById('DownButton').addEventListener('touchstart', () => document.getElementById('DownButton').setAttribute('aria-pressed', 'true'));
+document.getElementById('DownButton').addEventListener('touchend', () => document.getElementById('DownButton').setAttribute('aria-pressed', 'false'));
