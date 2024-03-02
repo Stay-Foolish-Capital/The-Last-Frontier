514e1b1972ecf102b3860f13784d7d11dad95f3f
Stay-Foolish-Capital <160165584+Stay-Foolish-Capital@users.noreply.github.com>
2024-03-03 06:17:44 +0900

    Update titleState.js

--- mario/code/titleState.js
+++ mario/code/titleState.js
@@ -76,7 +76,7 @@ Mario.TitleState.prototype.Draw = function(context) {
 };
 
 Mario.TitleState.prototype.CheckForChange = function(context) {
-    if (Enjine.KeyboardInput.IsKeyDown(Enjine.Keys.S)) {
+    if (Enjine.KeyboardInput.IsKeyDown(Enjine.Keys.S) || IsSButtonPressed()) {
         context.ChangeState(Mario.GlobalMapState);
     }
 };
